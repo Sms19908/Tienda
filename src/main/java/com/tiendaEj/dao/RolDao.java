f4e671291a220fae279a0f7ae4e04b94b0402884
Saul <Saul@DESKTOP-9UMRP2P>
2023-12-07 11:36:29 -0800

    Semana 11, creacion de apartado de Regsitro
    Finalizacion de Usuario, agregacion de registro y creacion de reportes en
    Jasperstudio

---
+++ src/main/java/com/tiendaEj/dao/RolDao.java
@@ -0,0 +1,9 @@
+
+package com.tiendaEj.dao;
+
+import com.tiendaEj.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
--- src/main/java/com/tiendaEj/dao/UsuarioDao.java
+++ src/main/java/com/tiendaEj/dao/UsuarioDao.java
@@ -7,4 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
     Usuario findByUsername(String username);
     /* Esta es como la de findByPrecio, pero busca entre los nombres de usuario */
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+
 }
